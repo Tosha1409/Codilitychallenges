66197436be5897e1d9e3cddcb0c3ff7a650c2df1
Anton Gurskiy <anton.gurskiy@gmail.com>
2019-12-17 22:59:25 +0200

    Add files via upload

--- Germanium2018.py
+++ Germanium2018.py
@@ -1,28 +1,29 @@
-coin,maximum,visited= [],[],[]
+card,maximum,visited= [],[],[]
 
-def find (x):
-    if (x!=coin[x]): coin[x]=find(coin[x])
-    return (coin[x])
+def find (x): #finding maximum and marking 
+    if (x!=card[x]): card[x]=find(card[x])
+    return (card[x])
 
 def solution(A, B):
-    n=len(A)
+    n=len(A) #initialization and filling arrays
     for x in range(0,n+1):
-        coin.append(x)
+        card.append(x)
         maximum.append(x)
         visited.append(False)
 
     for x in range(0,n):
-        if A[x]>n and B[x]>n: continue
-        elif A[x]>n or B[x]>n or (B[x]==A[x]):
+        if A[x]>n and B[x]>n: continue #pass if front and back side of card value too big
+        elif A[x]>n or B[x]>n or (B[x]==A[x]): #if only one side of card fits
             f=b=find(min(A[x],B[x]))
-        else: f,b=find(A[x]),find(B[x])
-            
-        if (f==b): visited[f]=1
+        else: f,b=find(A[x]),find(B[x]) #both sides of card fits
+        #main part that make changes    
+        if (f==b): visited[f]=True
         else:
-            coin[f]=b
+            card[f]=b
             if visited[f]==True: visited[b]=True 
             maximum[b]=max(maximum[f],maximum[b])
     
+    #finding final result
     result = n+1;
     for i in range (1,n+1): 
         if visited[find(i)]==False: result =min(result, maximum[find(i)])
